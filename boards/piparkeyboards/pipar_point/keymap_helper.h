85d5db992218a57ac3547d7683bce6ffbcc92386
rasmuskoit <rasmuskoit@gmail.com>
2025-11-02 05:32:21 +0200

    add some more to keymap

---
+++ boards/piparkeyboards/pipar_point/keymap_helper.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//    00 01 02 03 04       05 06 07 08 09
+// 10 11 12 13 14 15       16 17 18 19 20 21
+// 22 23 24 25 26 27       28 29 30 31 32 33
+//          34 35 36       37 38 39
+#define KEYS_LEFT   0 1 2 3 4 10 11 12 13 14 15 22 23 24 25 26 27
+#define KEYS_RIGHT  5 6 7 8 9 16 17 18 19 20 21 28 29 30 31 32 33
+#define KEYS_THUMBS 34 35 36 37 38 39
+#define UC_ET_OT  0xf5  0xd5    // ō/Ō
+#define UC_ET_AE  0xe4  0xc4    // ä/Ä
+#define UC_ET_OE  0xf6  0xd6    // ö/Ö
+#define UC_ET_UE  0xfc  0xdc    // ü/Ü
