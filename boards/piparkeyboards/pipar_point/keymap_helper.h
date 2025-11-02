a7dd61d56da5477e44fff8d97d13ac734653bf21
rasmuskoit <rasmuskoit@gmail.com>
2025-11-02 13:58:23 +0200

    ugh

--- boards/piparkeyboards/pipar_point/keymap_helper.h
+++ boards/piparkeyboards/pipar_point/keymap_helper.h
@@ -7,10 +7,10 @@
 #define KEYS_LEFT   0 1 2 3 4 10 11 12 13 14 15 22 23 24 25 26 27
 #define KEYS_RIGHT  5 6 7 8 9 16 17 18 19 20 21 28 29 30 31 32 33
 #define KEYS_THUMBS 34 35 36 37 38 39
-#define ET_OT  0xf5 0xd5    // ō/Ō
-#define ET_AE  0xe4 0xc4    // ä/Ä
-#define ET_OE  0xf6 0xd6    // ö/Ö
-#define ET_UE  0xfc 0xdc    // ü/Ü
+// #define ET_OT  0xf5 0xd5    // ō/Ō
+// #define ET_AE  0xe4 0xc4    // ä/Ä
+// #define ET_OE  0xf6 0xd6    // ö/Ö
+// #define ET_UE  0xfc 0xdc    // ü/Ü
 
 #define L_BASE   0
 #define L_NAV    1
