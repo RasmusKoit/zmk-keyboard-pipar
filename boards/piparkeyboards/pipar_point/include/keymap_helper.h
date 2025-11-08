62561bff46fe2551e61be69b7cb6c0611ba09846
rasmuskoit <rasmuskoit@gmail.com>
2025-11-08 16:14:33 +0200

    add pipar_point board metadata and keymap helper
    - add pipar_point.yaml and pipar_point.zmk.yml board metadata
    - add include/keymap_helper.h (key indexes, unicode aliases, layer ids)
    - update pipar_point.keymap include path to "include/keymap_helper.h"

---
+++ boards/piparkeyboards/pipar_point/include/keymap_helper.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//    00 01 02 03 04       05 06 07 08 09
+// 10 11 12 13 14 15       16 17 18 19 20 21
+// 22 23 24 25 26 27       28 29 30 31 32 33
+//          34 35 36       37 38 39
+#define KEYS_LEFT   0 1 2 3 4 10 11 12 13 14 15 22 23 24 25 26 27
+#define KEYS_RIGHT  5 6 7 8 9 16 17 18 19 20 21 28 29 30 31 32 33
+#define KEYS_THUMBS 34 35 36 37 38 39
+
+#define ET_OT  0xf5 0xd5    // ō/Ō
+#define ET_AE  0xe4 0xc4    // ä/Ä
+#define ET_OE  0xf6 0xd6    // ö/Ö
+#define ET_UE  0xfc 0xdc    // ü/Ü
+
+#define L_BASE   0
+#define L_NAV    1
+#define L_MOUSE  2
+#define L_MEDIA  3
+#define L_NUM    4
+#define L_SYM    5
+#define L_FUN    6
